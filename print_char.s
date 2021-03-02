32d7de5a0eeb7b89f78cb5e39e588f09a00119d1
Arto Rasimus <artorasimus@yahoo.com>
2021-03-02 11:48:11 +0200

    Add files via upload
    Initial commit

---
+++ print_char.s
@@ -0,0 +1,168 @@
+/* print_char.s 1.3.2021 */
+@ print_char.s
+@ Identify cpu, floating-point and syntax
+@.arch armv7l
+.cpu cortex-a53
+.fpu neon-fp-armv8
+.syntax unified
+.section .rodata
+.align 2
+@ ---------------------------------------
+@       Data Section
+@ ---------------------------------------
+.section .data
+str_function_name:
+    .asciz "print_char()\n"
+    strlen_function_name = .-str_function_name
+str_x:
+    .asciz "x"
+    strlen_x = .-str_x
+/*
+str_nl:
+    .asciz "\n"
+    strlen_nl = .-str_nl
+*/
+@ ---------------------------------------
+@       Block Starting Symbol Section
+@ ---------------------------------------
+@ The portion of an object that contains statically-allocated variables
+@ that are declared but have not been assigned a value yet
+
+/*
+.section .bss
+    .lcomm reg_tmp, 4     // 4 bytes for local common storage
+*/
+
+@ ---------------------------------------
+@       Code Section
+@ ---------------------------------------
+.section .text
+.align 2
+
+.global print_char
+.type print_char, %function
+print_char:
+/*
+    mov r0, $1            // syscall
+    ldr r1, =str_function_name // address of text string
+    ldr r2, =strlen_function_name // number of bytes to write
+    mov r7, $4            // SYS_WRITE = 4
+    swi 0
+*/
+    pop {r12}
+    mov r11, $8            // counter of 8 hex digits (32 bits)
+loop:
+    ror r12, $4            // bits 31, 30, 29, 28 go to bits 3, 2, 1, 0
+    and r9, r12, 0xF       // lowest 4 bits are masked in for following comparisons
+
+    mov r0, $48
+    cmp r9, $0            // 0x0000
+    beq print_hex_digit
+
+    mov r0, $49
+    cmp r9, $1            // 0x0001
+    beq print_hex_digit
+
+    mov r0, $50
+    cmp r9, $2            // 0x0002
+    beq print_hex_digit
+
+    mov r0, $51
+    cmp r9, $3            // 0x0003
+    beq print_hex_digit
+
+    mov r0, $52
+    cmp r9, $4            // 0x0004
+    beq print_hex_digit
+
+    mov r0, $53
+    cmp r9, $5            // 0x0005
+    beq print_hex_digit
+
+    mov r0, $54
+    cmp r9, $6            // 0x0006
+    beq print_hex_digit
+
+    mov r0, $55
+    cmp r9, $7            // 0x0007
+    beq print_hex_digit
+
+    mov r0, $56
+    cmp r9, $8            // 0x0008
+    beq print_hex_digit
+
+    mov r0, $57
+    cmp r9, $9            // 0x0009
+    beq print_hex_digit
+
+    mov r0, $65
+    cmp r9, $10           // 0x000A
+    beq print_hex_digit
+
+    mov r0, $66
+    cmp r9, $11           // 0x000B
+    beq print_hex_digit
+
+    mov r0, $67
+    cmp r9, $12           // 0x000C
+    beq print_hex_digit
+
+    mov r0, $68
+    cmp r9, $13           // 0x000D
+    beq print_hex_digit
+
+    mov r0, $69
+    cmp r9, $14           // 0x000E
+    beq print_hex_digit
+
+    mov r0, $70
+    cmp r9, $15           // 0x000F
+    beq print_hex_digit
+
+print_hex_digit:
+    mov r9, r0
+    ldr r10, =str_x       // just to get the address of the text string to r9
+    str r9, [r10]         /* address of r9 (i.e. start of the string)
+                             used for storing the ASCII char number */
+
+    mov r0, $1            // syscall
+    ldr r1, =str_x        // address of text string
+    ldr r2, =strlen_x     // number of bytes to write
+    mov r7, $4            // SYS_WRITE = 4
+    swi 0
+
+    sub r11, $1          // loop_count -= 1
+    cmp r11, $4          // if loop_count == 4
+    beq print_space      // then print_space
+    cmp r11, $0          // if loop_count > 0
+    bgt loop             // then continue loop
+    cmp r11, $0          // if loop_count == 0
+    beq nl               // then "\n"
+
+print_space:
+    mov r9, $32
+    ldr r10, =str_x       // just to get the address of the text string to r9
+    str r9, [r10]         /* address of r9 (i.e. start of the string)
+                             used for storing the ASCII char number */
+    mov r0, $1            // syscall
+    ldr r1, =str_x        // address of text string
+    ldr r2, =strlen_x     // number of bytes to write
+    mov r7, $4            // SYS_WRITE = 4
+    swi 0
+    b loop
+
+nl:
+    mov r9, $10           // CR
+    ldr r10, =str_x       // just to get the address of the text string to r9
+    str r9, [r10]         /* address of r9 (i.e. start of the string)
+                             used for storing the ASCII char number */
+
+    mov r0, $1            // syscall
+    ldr r1, =str_x       // address of text string
+    ldr r2, =strlen_x    // number of bytes to write
+    mov r7, $4            // SYS_WRITE = 4
+    swi 0
+
+end:
+    bx  lr
+
---
+++ read_input.s
@@ -0,0 +1,94 @@
+/* This function reads one character from stdin and accepts values 0..9.
+Value is returned in r5 as plain 1-byte digit. 
+'0' (ASCII 48) returned as 0
+'1' (ASCII 49) returned as 1
+'2' (ASCII 50) returned as 2
+'3' (ASCII 51) returned as 3
+'4' (ASCII 52) returned as 4
+'5' (ASCII 53) returned as 5
+'6' (ASCII 54) returned as 6
+'7' (ASCII 55) returned as 7
+'8' (ASCII 56) returned as 8
+'9' (ASCII 57) returned as 9
+TODO: better input handling. Now is read only one char.
+Later could be read until <enter>.
+Arto Rasimus 1.3.2021 */
+@ read_input.s
+@ reads input and returns it to _start
+@ Identify cpu, floating-point and syntax
+@.arch armv7l
+.cpu cortex-a53
+.fpu neon-fp-armv8
+.syntax unified
+.section .rodata
+.align 2
+@ ---------------------------------------
+@	Data Section
+@ ---------------------------------------
+.section .data
+message:
+        .asciz "Give number (0 = quit): "
+        strlen = .-message
+
+msg_err_wrong_input:
+        .asciz "Wrong input\n"
+        strlen_msg_err_wrong_input = .-msg_err_wrong_input
+
+@ ---------------------------------------
+@       Block Starting Symbol Section
+@ ---------------------------------------
+@ The portion of an object that contains statically-allocated variables
+@ that are declared but have not been assigned a value yet
+
+.section .bss
+    .lcomm times, 1     // 2 bytes for local common storage
+
+@ ---------------------------------------
+@	Code Section
+@ ---------------------------------------
+.section .text
+.align 2
+
+.global read_input
+read_input:
+.type read_input, %function
+begin_function:
+    // print question string
+    mov r0, $1         // syscall
+    ldr r1, =message   // address of text string
+    ldr r2, =strlen    // number of bytes to write
+    mov r7, $4         // SYS_WRITE = 4
+    swi 0
+
+    mov r0, $0         // syscall: SYS_READ
+    ldr r1, =times     // Load our reserved 3 bytes for the buffer into r4
+    mov r2, $1         // Set max input size to 1 byte: char
+    mov r7, $3         // Load syscall SYS_READ (3) into r7
+    swi $0             // Invoke the system call
+
+    ldr r5, [r1]       // save reference of character to r5
+    and r5, r5, $0x0FF // the enter is masked off
+
+begin_if:
+    // check input: 0 - 9
+    cmp r5, $48         // is < '0'
+    blt out_of_limits
+
+    cmp r5, $57         // is > '9'
+    bgt out_of_limits
+
+value_ok:
+    sub r5, $48         // value is valid. Convert ASCII to number
+    b end
+
+out_of_limits:
+    mov r0, $1        // syscall
+    ldr r1, =msg_err_wrong_input // address of text string
+    ldr r2, =strlen_msg_err_wrong_input  // number of bytes to write
+    mov r7, $4          // SYS_WRITE = 4
+    swi 0
+
+    b begin_function    // invalid character
+
+end:
+    bx  lr
